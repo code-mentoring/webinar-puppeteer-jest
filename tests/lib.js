a593699e20d7385244917750d7eb70c8b8f8601b
Tristan Matthias <hello@tristanmatthias.com>
2020-06-18 19:55:28 -0400

    fix: updated utils

--- tests/lib.js
+++ tests/lib.js
@@ -2,9 +2,16 @@
  * Get the computed styles for a element
  * @param {string} selector Query Selector of element
  */
-export const getStyles = (selector) =>
+module.exports.getStyles = (selector) =>
   // Run the JS "inside" the Puppeteer page
   page.evaluate(selector => {
     // Run inside the page, NOT in Jest
-    return window.getComputedStyle(document.querySelector(selector));
+    const ele = document.querySelector(selector);
+
+    // if (!ele) throw new Error(`Could not find element with selector '${selector}'`);
+    if (!ele) return null;
+
+    // Convert to plain JSON, not the fancy CSSRules
+    return JSON.parse(JSON.stringify(window.getComputedStyle(ele)));
+
   }, selector)
