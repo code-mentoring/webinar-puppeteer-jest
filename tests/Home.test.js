9e10c93ec8de2c9f08c87b7d50fb5b5fb0b7166d
Tristan Matthias <hello@tristanmatthias.com>
2020-06-18 19:37:33 -0400

    feat: added basic tests

--- tests/Home.test.js
+++ tests/Home.test.js
@@ -1,12 +1,30 @@
 require('expect-puppeteer');
 
 
-describe('Google', () => {
+describe('Increment button', () => {
   beforeAll(async () => {
-    await page.goto('https://google.com')
+    await page.goto('http://localhost:8080/')
   })
 
-  it('should display "google" text on page', async () => {
-    await expect(page).toMatch('google')
-  })
+  it('#number-field should start at 0', async () => {
+    const field = await page.$('#number-field');
+    await expect(field).toMatch('0');
+  });
+
+  it('#number-button should have text "Increment"', async () => {
+    const button = await page.$('#number-button');
+    await expect(button).toMatch('Increment');
+  });
+
+  it('#number-button should increment #number-field', async () => {
+    await page.click('#number-button');
+    await expect(page).toMatchElement('#number-field', {
+      text: '1'
+    });
+
+    await page.click('#number-button');
+    await expect(page).toMatchElement('#number-field', {
+      text: '2'
+    })
+  });
 })
